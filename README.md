efb9d16c3e086d542922d6cf449038f98c6c19a6
Bas de Ruiter <deruiterb@gmail.com>
2022-12-30 15:09:13 +0100

    :tada: Initialize Netflix Laravel API

---
+++ README.md
@@ -0,0 +1,5 @@
+# Netflix laravel API
+
+This is the backend API for a [React SPA](https://github.com/winkbrace/netflix-react).
+
+This connects to rapidapi to fetch Netflix movies and store it in a local database for the React SPA to consume.
---
+++ src/Content/Importer.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Netflix\Content;
+
+/**
+ * This class is responsible for importing netflix movies and series into a local database.
+ * This is required because in the freemium version there is a tiny request limit.
+ */
+final class Importer
+{
+
+}
